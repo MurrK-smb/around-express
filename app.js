f6e21ae4cbb248c4874364a89c802672a7c03bcc
NicolasGil <nick.gil360@gmail.com>
2022-03-11 02:52:45 -0500

    add db

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
 const helmet = require('helmet')
 
 const { PORT = 5000, BASE_PATH } = process.env
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/mydb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
 
 app.use(helmet())
 app.use(bodyParser.json())
