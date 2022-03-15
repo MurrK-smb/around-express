0929c7b1dd32d62cd52e3285ff80c17892d12fa7
NicolasGil <nick.gil360@gmail.com>
2022-03-15 07:28:28 -0400

    update routes/cards.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const routes = require('./routes')
+const helmet = require('helmet')
+
+const { PORT = 5000, BASE_PATH } = process.env
+const app = express()
+
+mongoose.connect('mongodb://localhost:27017/aroundb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', routes)
+
+app.listen(PORT, () => {
+  console.log('ok')
+  console.log(BASE_PATH)
+})
--- routes/cards.js
+++ routes/cards.js
@@ -1,7 +1,17 @@
 const router = require('express').Router()
 
-const { getCards } = require('../controllers/cards')
+const { 
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard
+ } = require('../controllers/cards')
 
 router.get('/', getCards)
+router.get('/', createCard)
+router.get('/:id', deleteCard)
+router.get('/:id/likes', likeCard)
+router.get('/:id/likes', dislikeCard)
 
 module.exports = router
