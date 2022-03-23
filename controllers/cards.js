154456d9a153ca8e0fba8dcb924f7069989d172a
NicolasGil <nick.gil360@gmail.com>
2022-03-23 06:23:14 -0400

    refactor

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,30 +1,34 @@
-const Card = require('../models/card');
+const Card = require("../models/card");
 const {
   SUCCESS,
   CREATED_SUCCESS,
   BAD_REQUEST_ERROR,
   NOT_FOUND_ERROR,
   INTERNAL_SERVER_ERROR,
-} = require('../utils/constants');
+} = require("../utils/constants");
 
 const getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.status(SUCCESS).send({ data: cards }))
-    .catch(() => res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error occurred' }));
+    .catch(() =>
+      res.status(INTERNAL_SERVER_ERROR).send({ message: "An error occurred" })
+    );
 };
 
 const createCard = (req, res) => {
   const owner = req.user._id;
   const { name, link } = req.body;
   Card.create({ name, link, owner })
-    .then((card) => {
-      res.status(CREATED_SUCCESS).send({ data: card });
-    })
+    .then((card) => res.status(CREATED_SUCCESS).send({ data: card }))
     .catch((err) => {
-      if (err.name === 'Validation Error') {
-        res.status(BAD_REQUEST_ERROR).send({ message: `${Object.values}` });
+      if (err.name === "ValidationError") {
+        return res
+          .status(BAD_REQUEST_ERROR)
+          .send({ message: "Data entered incorrectly" });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error has occurred' });
+        return res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred" });
       }
     });
 };
@@ -33,28 +37,36 @@ const deleteCard = (req, res) => {
   const { id } = req.params;
   Card.findById(id)
     .orFail(() => {
-      const error = new Error('No card found for the specified id');
+      const error = new Error("No card found for the specified id");
       error.statusCode = NOT_FOUND_ERROR;
       throw error;
     })
     .then((card) => Card.deleteOne(card))
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR).send({ message: 'Invalid card Id' });
+      if (err.name === "CastError") {
+        res.status(BAD_REQUEST_ERROR).send({ message: "Invalid card Id" });
       } else if (err.statusCode === NOT_FOUND_ERROR) {
         res.status(NOT_FOUND_ERROR).send({ message: err.message });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error has occurred' });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred" });
       }
     });
 };
 
 const updateLike = (req, res, method) => {
-  const { params: { id } } = req;
-  Card.findByIdAndUpdate(id, { [method]: { likes: req.user._id } }, { new: true })
+  const {
+    params: { id },
+  } = req;
+  Card.findByIdAndUpdate(
+    id,
+    { [method]: { likes: req.user._id } },
+    { new: true }
+  )
     .orFail(() => {
-      const error = new Error('Id did not match existing card');
+      const error = new Error("Id did not match existing card");
       error.statusCode = NOT_FOUND_ERROR;
       throw error;
     })
@@ -62,19 +74,21 @@ const updateLike = (req, res, method) => {
       res.send({ data: card });
     })
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR).send({ message: 'Invalid Id' });
+      if (err.name === "CastError") {
+        res.status(BAD_REQUEST_ERROR).send({ message: "Invalid Id" });
       } else if (err.statusCode === NOT_FOUND_ERROR) {
         res.status(NOT_FOUND_ERROR).send({ message: err.message });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error has occurred' });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred" });
       }
     });
 };
 
-const likeCard = (req, res) => updateLike(req, res, '$addToSet');
+const likeCard = (req, res) => updateLike(req, res, "$addToSet");
 
-const dislikeCard = (req, res) => updateLike(req, res, '$pull');
+const dislikeCard = (req, res) => updateLike(req, res, "$pull");
 
 module.exports = {
   getCards,
--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,38 @@
-const User = require('../models/user');
+const User = require("../models/user");
 const {
   CREATED_SUCCESS,
   BAD_REQUEST_ERROR,
   UNAUTHORIZED_ERROR,
   NOT_FOUND_ERROR,
   INTERNAL_SERVER_ERROR,
-} = require('../utils/constants');
+} = require("../utils/constants");
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch(() => res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error occurred' }));
+    .catch(() =>
+      res.status(INTERNAL_SERVER_ERROR).send({ message: "An error occurred" })
+    );
 };
 
 const getUser = (req, res) => {
   const { id } = req.params;
   User.findById(id)
     .orFail(() => {
-      const error = new Error('User Id not found');
+      const error = new Error("User Id not found");
       error.statusCode = NOT_FOUND_ERROR;
       throw error;
     })
     .then((users) => res.send({ data: users }))
     .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR).send({ message: 'Invalid user Id' });
+      if (err.name === "CastError") {
+        res.status(BAD_REQUEST_ERROR).send({ message: "Invalid user Id" });
       } else if (err.statusCode === NOT_FOUND_ERROR) {
         res.status(NOT_FOUND_ERROR).send({ message: err.message });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error has occurred on the server' });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server" });
       }
     });
 };
@@ -38,12 +42,16 @@ const createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.status(CREATED_SUCCESS).send({ data: user }))
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === "ValidationError") {
         res.status(BAD_REQUEST_ERROR).send({
-          message: `${Object.values(err.errors).map((error) => error.message).join(', ')}`,
+          message: `${Object.values(err.errors)
+            .map((error) => error.message)
+            .join(", ")}`,
         });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error has occurred on the server' });
+        res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server" });
       }
     });
 };
@@ -52,20 +60,32 @@ const updateUserInfo = (req, res) => {
   const { name, about } = req.body;
   const id = req.user._id;
 
-  User.findByIdAndUpdate(id, { name, about }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(
+    id,
+    { name, about },
+    { new: true, runValidators: true }
+  )
     .orFail(() => {
-      const error = new Error('User Id not found');
+      const error = new Error("User Id not found");
       error.statusCode = NOT_FOUND_ERROR;
       throw error;
     })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      if (err.name === 'Validation Error') {
-        res.status(UNAUTHORIZED_ERROR).send({ message: `${Object.values(err.errors).map((error) => error.message).join(', ')}` });
-      } else if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR).send({ message: 'Invalid user Id' });
+      if (err.name === "ValidationError") {
+        return res.status(UNAUTHORIZED_ERROR).send({
+          message: `${Object.values(err.errors)
+            .map((error) => error.message)
+            .join(", ")}`,
+        });
+      } else if (err.name === "CastError") {
+        return res
+          .status(BAD_REQUEST_ERROR)
+          .send({ message: "Invalid user Id" });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error has occurred on the server' });
+        return res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server" });
       }
     });
 };
@@ -73,27 +93,22 @@ const updateUserInfo = (req, res) => {
 const updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
   const id = req.user._id;
-  User.findByIdAndUpdate(id, { avatar }, { new: true });
-  const error = new Error('Avatar Id not found')
+  User.findByIdAndUpdate(id, { avatar }, { new: true, runValidators: true })
     .orFail(() => {
+      const error = new Error("Avatar Id not found");
       error.statusCode = NOT_FOUND_ERROR;
       throw error;
     })
-    .then((user) => {
-      console.error();
-      if (!user) {
-        throw error;
-      } else {
-        res.status(CREATED_SUCCESS).send({ data: user });
-      }
-    })
+    .then((user) => res.status(CREATED_SUCCESS).send({ data: user }))
     .catch((err) => {
-      if (err.name === 'DocumentNotFound') {
-        res.status(NOT_FOUND_ERROR).send({ message: 'Id not found' });
-      } else if (err.name === 'CastError') {
-        res.status(BAD_REQUEST_ERROR).send({ message: 'Invalid Id' });
+      if (err.name === "DocumentNotFound") {
+        return res.status(NOT_FOUND_ERROR).send({ message: "Id not found" });
+      } else if (err.name === "CastError") {
+        return res.status(BAD_REQUEST_ERROR).send({ message: "Invalid Id" });
       } else {
-        res.status(INTERNAL_SERVER_ERROR).send({ message: 'An error has occurred on the server' });
+        return res
+          .status(INTERNAL_SERVER_ERROR)
+          .send({ message: "An error has occurred on the server" });
       }
     });
 };
